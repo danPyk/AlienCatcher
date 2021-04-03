be1a7971fbf2cafdcf77b30182c24bd2a004eb90
danPyk <dan.pykalo@gmail.com>
2021-04-03 11:30:23 +0200

    work fine

--- app/src/main/java/com/whayway/beerrandom/game/GameFragment.kt
+++ app/src/main/java/com/whayway/beerrandom/game/GameFragment.kt
@@ -2,10 +2,11 @@ package com.whayway.beerrandom.game
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.Handler
-import android.view.*
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
